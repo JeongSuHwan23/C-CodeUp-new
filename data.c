9c64fe289bfd627dea483f77709e6751b57d2818
JungSuHwan23 <short9666@gmail.com>
2023-09-10 21:31:05 +0900

    09-10

--- data.c
+++ data.c
@@ -26,3 +26,52 @@ int main() {
         printf("%d\n", a[i]);
     return 0;
 }
+
+
+//2023-09-10
+
+//codeup 1442 선택
+#include <stdio.h>
+int a[10001];
+int n, i, j, temp, min;
+int main() {
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    for (i=1; i<n; i++) {
+        min=i;
+        for (j=i+1; j<=n; j++) {
+            if(a[j]<a[min])
+                min = j;
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+    for (i=1; i<=n; i++)
+        printf("%d\n", a[i]);
+    return 0;
+}
+
+//codeup 1443 삽입
+#include <stdio.h>
+int a[10001];
+int n, i, j, temp, key;
+int main() {
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+    for (i=2; i<=n; i++)
+    {
+        key=a[i];
+        for(j=i-1; j>=0 && key<a[j]; j--)
+        {
+            a[j+1]=a[j];
+        }
+        a[j+1]=key;
+    }
+
+    for (i=1; i<=n; i++)
+        printf("%d\n", a[i]);
+    return 0;
+}
